52f6cd02307bfd75b39162b1cbbd250452f751e4
vfor4 <trandangvu2708@gmail.com>
2025-02-15 21:13:19 +0700

    hi

--- uds_test.go
+++ uds_test.go
@@ -9,7 +9,87 @@ import (
 	"testing"
 )
 
-func TestUnitStreaming(t *testing.T) {
+func TestUnixDatagram(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	dir, err := os.MkdirTemp("", "unix_data_echo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		os.RemoveAll(dir)
+	}()
+	lsocket := filepath.Join(dir, fmt.Sprintf("l%d.sock", os.Getpid()))
+	listenerAddr, err := unixDatagramEchoServer(ctx, "unixgram", lsocket)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Chmod(lsocket, os.ModeSocket|0622)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer cancel()
+
+	ssocket := filepath.Join(dir, fmt.Sprintf("s%d.sock", os.Getpid()))
+	conn, err := net.ListenPacket("unixgram", ssocket)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = os.Chmod(ssocket, os.ModeSocket|0662)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 3; i++ {
+		_, err = conn.WriteTo([]byte("ping"), listenerAddr)
+		if err != nil {
+			t.Log("Failed to write at i:", i+1)
+		}
+	}
+	buf := make([]byte, 1024)
+	for i := 0; i < 3; i++ {
+		n, _, err := conn.ReadFrom(buf)
+		if err != nil {
+			t.Log("Failed to read")
+		}
+		t.Log(string(buf[:n]))
+	}
+}
+
+func unixDatagramEchoServer(ctx context.Context, network, address string) (net.Addr, error) {
+	conn, err := net.ListenPacket(network, address)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer func() {
+			<-ctx.Done()
+			conn.Close()
+
+		}()
+		b := make([]byte, 1024)
+		for {
+			if err != nil {
+				return
+			}
+			go func() {
+				rn, readAddr, err := conn.ReadFrom(b)
+				fmt.Println("reading...")
+				if err != nil {
+					return
+				}
+				_, err = conn.WriteTo(b[:rn], readAddr)
+				if err != nil {
+					return
+				}
+			}()
+		}
+	}()
+	return conn.LocalAddr(), nil
+}
+
+func xTestUnitStreaming(t *testing.T) {
 	dir, err := os.MkdirTemp("", "echo_unix")
 	if err != nil {
 		t.Fatal(err)
@@ -67,7 +147,7 @@ func streamingClient(ctx context.Context, network, address string) error {
 func streamingEchoServer(ctx context.Context, network, address string) (net.Addr, error) {
 	unixPath := &net.UnixAddr{Name: address, Net: "unix"}
 	listener, err := net.ListenUnix(network, unixPath)
-	listener.SetUnlinkOnClose(false)
+	listener.SetUnlinkOnClose(true)
 	if err != nil {
 		return nil, err
 	}
